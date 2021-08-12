c5b991e22937f4b0fcec474160368b7f9112cf53
extremepayne <harrison.payne@gmail.com>
2021-08-12 00:18:05 +0000

    Allow multiple inputs in one run

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ INPUT_FILE_PATH = "tokipona.json"
 with open(INPUT_FILE_PATH, "r") as input_file:
     toki_pona_dict = json.load(input_file)
 
-toki_phrase = input("Enter toki pona phrase: ")
-words = toki_phrase.split(" ")
-output = ""
-for word in words:
-    if word in toki_pona_dict:
-        output += toki_pona_dict[word]
-    else:
-        output += word
-    output += " "
+toki_phrase = ""
 
-print(output)
+while True:
+    toki_phrase = input("Enter toki pona phrase (q to quit):\n")
+    if toki_phrase == "q":
+        break
+    words = toki_phrase.split(" ")
+    output = ""
+    for word in words:
+        if word in toki_pona_dict:
+            output += toki_pona_dict[word]
+        else:
+            output += word
+        output += " "
+
+    print("Translation:", output, sep="\n")
